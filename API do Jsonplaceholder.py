ebe07ef9337f6b229240879e1eefba6d248db1f2
caroline.oliveira6 <carolineraqueldeoliveira@gmail.com>
2025-05-16 20:45:56 -0300

    adiciona função de login com verificação de credenciais

--- API do Jsonplaceholder.py
+++ API do Jsonplaceholder.py
@@ -45,6 +45,27 @@ def menu_inicial():
 menu_inicial
 
 
+# 4. Função de login
+def fazer_login():
+    print("\n--- Login de Usuário ---")
+    email = input("Digite seu e-mail: ")
+    senha = input("Digite sua senha: ")
+
+    if email in usuarios and usuarios[email]["senha"] == senha:
+        print(f"Login realizado com sucesso! Bem-vindo(a), {usuarios[email]['nome']}.\n")
+        return usuarios[email]  # Retorna os dados do usuário logado
+    else:
+        print("E-mail ou senha inválidos. Tente novamente.\n")
+        return None
+
+
+# Reutiliza o menu para tentar login até funcionar
+usuario_logado = None
+while not usuario_logado:
+    if menu_inicial() == "login":
+        usuario_logado = fazer_login()
+
+
 
 
 
